c4cfa1cfeb0f642b2a014982a7cadc969ca289cc
Timo Nussbaum <timo.nussbaum@gmx.de>
2021-01-06 22:10:42 +0100

    Task: Add BloggingCommandHandlerTest.

--- tests/Infrastructure/Domain/Context/Blogging/BloggingCommandHandlerTest.php
+++ tests/Infrastructure/Domain/Context/Blogging/BloggingCommandHandlerTest.php
@@ -1,10 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Domain\Context\Blogging;
 
 use App\Domain\Context\Blogging\BloggingCommandHandler;
 use App\Domain\Context\Blogging\Command\CreateBlog;
 use App\Domain\Projection\User\UserIdentifier;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class BloggingCommandHandlerTest extends KernelTestCase
@@ -14,6 +17,11 @@ class BloggingCommandHandlerTest extends KernelTestCase
      */
     private $bloggingCommandHandler;
 
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
     public function setUp()
     {
         parent::setUp();
@@ -22,7 +30,9 @@ class BloggingCommandHandlerTest extends KernelTestCase
         $container = $kernel->getContainer();
 
         $this->bloggingCommandHandler = $container->get(BloggingCommandHandler::class);
-
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
     }
 
     /**
@@ -30,14 +40,49 @@ class BloggingCommandHandlerTest extends KernelTestCase
      */
     public function handleCreateBlogReturnsTheExpectedResult()
     {
+        // given a create blog command
         $command = new CreateBlog(
             'Minecraft',
             UserIdentifier::fromString('Mr. Author')
         );
 
+        // when the event is stored
         $this->bloggingCommandHandler->handleCreateBlog($command);
 
-        $this->assertEquals(1,1);
+        $query = "
+            SELECT 
+              name,     
+              COUNT(*) AS NumberOfBlogs
+            FROM blog
+            GROUP BY name
+        ";
+
+        $result = $this->entityManager
+            ->getConnection()
+            ->query($query)
+            ->fetch();
+
+        // then the given name is returned
+        $this->assertEquals(
+            1,
+            $result['NumberOfBlogs']
+        );
+
+        $this->assertEquals(
+            'Minecraft',
+            $result['name']
+        );
     }
 
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        $query = "
+            Truncate table blog;
+            Truncate table blog_events;
+            Truncate table neos_eventsourcing_eventlistener_appliedeventslog
+        ";
+        $this->entityManager->getConnection()->exec($query);
+    }
 }
